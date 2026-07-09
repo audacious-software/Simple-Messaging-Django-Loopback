a33ebfdba4433a2dd3c64b3455ad026d6043f54b
Chris J. Karr <chris@audacious-software.com>
2026-07-09 17:24:44 -0500

    Adding switchboard config checks

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
-# pylint: disable=line-too-long
+# pylint: disable=line-too-long, no-member
+
+from django.conf import settings
+from django.core.checks import Warning, register # pylint: disable=redefined-builtin
+
+@register()
+def check_loopback_settings_defined(app_configs, **kwargs): # pylint: disable=unused-argument
+    errors = []
+
+    if 'simple_messaging_switchboard' in settings.INSTALLED_APPS:
+        from simple_messaging_switchboard.models import Channel # pylint: disable=import-outside-toplevel
+
+        count = Channel.objects.filter(channel_type__package_name='simple_messaging_loopback').count()
+
+        if count == 0:
+            error = Warning('simple_messaging_loopback is installed alongside simple_messaging_switchboard, but no Channels are defined.', hint='Create Channel or consider removing simple_messaging_loopback from settings.INSTALLED_APPS', obj=None, id='simple_messaging_lopback.E001')
+            errors.append(error)
+
+    return errors
