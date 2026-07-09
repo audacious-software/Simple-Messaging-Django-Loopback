5ea8efffdf1322c053ac2bd1bd266584ce7403fb
Chris J. Karr <chris@audacious-software.com>
2026-07-09 17:52:20 -0500

    CI fix for check before migrations

--- models.py
+++ models.py
@@ -8,12 +8,15 @@ def check_loopback_settings_defined(app_configs, **kwargs): # pylint: disable=un
     errors = []
 
     if 'simple_messaging_switchboard' in settings.INSTALLED_APPS:
-        from simple_messaging_switchboard.models import Channel # pylint: disable=import-outside-toplevel, import-error
+        try:
+            from simple_messaging_switchboard.models import Channel # pylint: disable=import-outside-toplevel, import-error
 
-        count = Channel.objects.filter(channel_type__package_name='simple_messaging_loopback').count()
+            count = Channel.objects.filter(channel_type__package_name='simple_messaging_loopback').count()
 
-        if count == 0:
-            error = Warning('simple_messaging_loopback is installed alongside simple_messaging_switchboard, but no Channels are defined.', hint='Create Channel or consider removing simple_messaging_loopback from settings.INSTALLED_APPS', obj=None, id='simple_messaging_lopback.E001')
-            errors.append(error)
+            if count == 0:
+                error = Warning('simple_messaging_loopback is installed alongside simple_messaging_switchboard, but no Channels are defined.', hint='Create Channel or consider removing simple_messaging_loopback from settings.INSTALLED_APPS', obj=None, id='simple_messaging_lopback.E001')
+                errors.append(error)
+        except ProgrammingError:
+            pass # Migrations not applied
 
     return errors
